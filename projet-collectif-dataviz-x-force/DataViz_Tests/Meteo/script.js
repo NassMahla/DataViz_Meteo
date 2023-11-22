97b5b9f0547dc38bd3dafb4efb8414db6ee90412
nicotine189 <106276527+nicotine189@users.noreply.github.com>
2023-11-22 16:46:18 +0100

    ajouts du 22_11

--- projet-collectif-dataviz-x-force/DataViz_Tests/Meteo/script.js
+++ projet-collectif-dataviz-x-force/DataViz_Tests/Meteo/script.js
@@ -1,23 +1,25 @@
 const API_KEY = "554e3c56cfb28bccd863b1120534404e";
-const CITY = "";
-const lat = 0
-const lon = 0
-const API_URL_MAIN = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
-const API_GEO_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${CITY}&limit=1&appid=${API_KEY}`;
+let city = ""
+let lat = 0
+let lon = 0
+const API_URL_MAIN = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric `;
+const API_GEO_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`;
 
 document.querySelector("#search").addEventListener("click", getWeather);
 
-function geoCoding() {
+
+
+function geoCoding(city) {
+  const API_GEO_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`
+  console.log(city)
   fetch(API_GEO_URL)
   .then((response) => response.json())
   .then((data) => {
-    const lat = data.lat;
-    console.log(lat)
-    const lon = data.lon;
-    console.log(lon)
+    lat = data[0].lat;
+    lon = data[0].lon;
     document.querySelector(".meteoBox").innerHTML = `
     <div>
-    <p>Latitude: ${console.log(lat)}</p>
+    <p>Latitude: ${lat}</p>
     <p>Longitude: ${lon}</p>   
     </div>`
   })
@@ -28,38 +30,71 @@ function geoCoding() {
 
 
 
-function getWeather(e) {
-  const CITY = document.querySelector("#cityName").value;
-  console.log(fetch(API_GEO_URL).then((response) => response.json()))
-  
-  geoCoding(CITY)
-
-  // console.log(fetch(API_URL_MAIN).then((response) => response.json()))
-  .then(console.log(lat))
-  .then(console.log(lon))
-
-  .then(fetch(API_URL_MAIN))
-  .then((response) => response.json())
-  .then((data) => {
-      document.querySelector(".meteoBox").innerHTML = `
+async function getWeather(e) {
+  city = document.querySelector("#cityName").value;
+  await geoCoding(city)
+  console.log(lat)
+  console.log(lon)
+  try {
+    const response = await fetch(API_URL_MAIN);
+    const data = await response.json();
+    console.log(data.main.pressure)
+    console.log(data.main.temp)
+    document.querySelector(".meteoBox").innerHTML = `
       <div>
-      <img
-      src="${data.sprites.other["official-artwork"].front_default}"
-      alt="City name"
-      /> 
       </div>  
       <div class="cityInfos">
-      <h1>${capitalizeFirstLetter(data.name)}</h3>
+      <h1>'Test Affichage'</h1>
+      <h1>${(data.name)}</h1>
       <p>Température: ${data.main.temp}</p>
       <p>Humidité: ${data.main.humidity}</p>
       </div>`;
-  })
-  .catch((err) => {
-      document.querySelector(".meteoBox").innerHTML = `
+  } catch (err) {
+    document.querySelector(".meteoBox").innerHTML = `
       <h4>City not found 😞</h4>
       `;
-      console.log("City not found", err);
-  });
-  
+    console.log("City not found", err);
+  }
   e.preventDefault();
 }
+
+
+
+// async function getWeather(e) {
+
+//   city = document.querySelector("#cityName").value;
+//   await geoCoding(city)
+
+//   console.log(lat)
+//   console.log(lon)
+
+//   // console.log(fetch(API_URL_MAIN).then((response) => response.json()))
+//   .then(console.log(lat))
+//   .then(console.log(lon))
+
+//   .then(fetch(API_URL_MAIN))
+//   .then((response) => response.json())
+//   .then((data) => {
+//       document.querySelector(".meteoBox").innerHTML = `
+//       <div>
+//       <img
+//       src="${data.sprites.other["official-artwork"].front_default}"
+//       alt="City name"
+//       /> 
+//       </div>  
+//       <div class="cityInfos">
+//       <h1>${capitalizeFirstLetter(data.name)}</h3>
+//       <p>Température: ${data.main.temp}</p>
+//       <p>Humidité: ${data.main.humidity}</p>
+//       </div>`;
+//   })
+//   .catch((err) => {
+//       document.querySelector(".meteoBox").innerHTML = `
+//       <h4>City not found 😞</h4>
+//       `;
+//       console.log("City not found", err);
+//   });
+  
+//   e.preventDefault();
+// }
+
